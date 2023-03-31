0aefae769d09ef650460edb291f28d358fc8b9a1
Bychan <harrrypoter@naver.com>
2023-03-31 16:10:17 +0900

    Feat: 컴포넌트 병합

--- front/src/App.js
+++ front/src/App.js
@@ -1,38 +1,37 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MainPage from './Pages/MainPage';
-import RoomListPage from './Pages/RoomListPage';
-import RoomInformationPage from './Pages/RoomInformationPage';
-import ChangePasswordPage from './Pages/ChangePasswordPage';
-import ChangeCompletePage from './Pages/ChangeCompletePage';
-import ReservationPage from './Pages/ReservationPage';
-import ChooseReservationPage from './Pages/ChooseReservationPage';
-import ReservationCompletePage from './Pages/ReservationCompletePage';
-import ShareReservationPage from './Pages/ShareReservationPage';
+import Home from './components/pages/Home';
+import RoomListPage from './components/pages/RoomListPage';
+import RoomInformationPage from './components/pages/RoomInformationPage';
+import ChangePasswordPage from './components/pages/ChangePasswordPage';
+import ChangeCompletePage from './components/pages/ChangeCompletePage';
+import ReservationPage from './components/pages/ReservationPage';
+import ChooseReservationPage from './components/pages/ChooseReservationPage';
+import ReservationCompletePage from './components/pages/ReservationCompletePage';
+import ShareReservationPage from './components/pages/ShareReservationPage';
+import StartPage from "./components/pages/StartPage";
+import LoginPage from "./components/pages/LoginPage";
+import MainPage from "./components/pages/MainPage";
+import MyPage from "./components/pages/MyPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Routes>
-          <Route exact path="/" element={<MainPage />}>
-          </Route>
-          <Route path="/RoomListPage" element={<RoomListPage />}>
-          </Route>
-          <Route path="/RoomInformationPage" element={<RoomInformationPage />}>
-          </Route>
-          <Route path="/ChangePasswordPage" element={<ChangePasswordPage />}>
-          </Route>
-          <Route path="/ChangeCompletePage" element={<ChangeCompletePage />}>
-          </Route>
-          <Route path="/ReservationPage" element={<ReservationPage />}>
-          </Route>
-          <Route path="/ChooseReservationPage" element={<ChooseReservationPage />}>
-          </Route>
-          <Route path="/ReservationCompletePage" element={<ReservationCompletePage />}>
-          </Route>
-          <Route path="/ShareReservationPage" element={<ShareReservationPage />}>
-          </Route>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/RoomListPage" element={<RoomListPage />}></Route>
+          <Route path="/RoomInformationPage" element={<RoomInformationPage />}></Route>
+          <Route path="/ChangePasswordPage" element={<ChangePasswordPage />}></Route>
+          <Route path="/ChangeCompletePage" element={<ChangeCompletePage />}></Route>
+          <Route path="/ReservationPage" element={<ReservationPage />}></Route>
+          <Route path="/ChooseReservationPage" element={<ChooseReservationPage />}></Route>
+          <Route path="/ReservationCompletePage" element={<ReservationCompletePage />}></Route>
+          <Route path="/ShareReservationPage" element={<ShareReservationPage />}></Route>
+          <Route path="/startPage" element={<StartPage />}></Route>
+          <Route path="/loginPage" element={<LoginPage />}></Route>
+          <Route path="/mainPage" element={<MainPage />}></Route>
+          <Route path="/myPage" element={<MyPage />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
