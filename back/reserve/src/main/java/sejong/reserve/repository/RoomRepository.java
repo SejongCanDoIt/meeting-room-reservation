d043c88ce1fa0e7827b9d9a0001b91d77dc369e3
Dingadung <1229jimin@sju.ac.kr>
2023-04-10 01:34:21 +0900

    Edit: room, management updateans dirty check 유형으로 변경

--- back/reserve/src/main/java/sejong/reserve/repository/RoomRepository.java
+++ back/reserve/src/main/java/sejong/reserve/repository/RoomRepository.java
@@ -12,35 +12,31 @@ import static sejong.reserve.domain.Room.setRoom;
 
 @Repository
 @RequiredArgsConstructor
-public class RoomRepository implements Repo<Room> {
+public class RoomRepository{
     private final EntityManager em;
 
-    @Override
     public void save(Room room) {
         em.persist(room);
     }
 
-    @Override
     public Room findById(Long id) {
         return em.find(Room.class, id);
     }
 
-    @Override
     public List<Room> findAll() {
         return em.createQuery("select m from Room m", Room.class)
                 .getResultList();
     }
 
-    @Override
     public void delete(Long id) throws PersistenceException {
         Room room = findById(id);
         em.remove(room);
     }
 
-    @Override
-    public void update(Room roomInfo) throws PersistenceException {
-        Room room = em.find(Room.class, roomInfo.getId());
-        room.setRoom(roomInfo, room);
-    }
+//    @Override
+//    public void update(Room roomInfo) throws PersistenceException {
+//        Room room = em.find(Room.class, roomInfo.getId());
+//        room.setRoom(roomInfo, room);
+//    }
 
 }
