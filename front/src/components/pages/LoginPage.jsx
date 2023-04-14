fd24f3ab6f90b329ea6895d46ec867d24d56975f
Dingadung <1229jimin@sju.ac.kr>
2023-04-14 16:28:07 +0900

    Edit: checkLogin return 값 변경

--- front/src/components/pages/LoginPage.jsx
+++ front/src/components/pages/LoginPage.jsx
@@ -41,14 +41,15 @@ export default function LoginPage() {
         //     navigate('/myPage');
         // }
 
+        console.log("상태확인");
         // 서버로부터 로그인 여부 확인
-        // axios.get('/auth/checkLogin')
-        //     .then((res) => {
-
-        //     })
-        //     .catch((err) => {
-
-        //     })
+        axios.get('/auth/checkLogin')
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }, []);
 
     const onLoginIdHandler = (e) => {
