92bac69b97b69acb4ea9eb9a69654aea2dcf51b3
Dingadung <1229jimin@sju.ac.kr>
2023-04-28 12:41:42 +0900

    Edit: Room domain, Member domain

--- back/reserve/src/main/java/sejong/reserve/dto/MemberDto.java
+++ back/reserve/src/main/java/sejong/reserve/dto/MemberDto.java
@@ -3,7 +3,6 @@ package sejong.reserve.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import sejong.reserve.domain.AuthState;
 import sejong.reserve.domain.Member;
 
 @Getter
@@ -11,23 +10,11 @@ import sejong.reserve.domain.Member;
 @ToString
 public class MemberDto {
     private Long id; // 예약자 번호
-    private String major; // 전공
     private String studentNo; // 학번 (Login-ID)
-    private String phoneNo; // 전화번호
-    private AuthState authority; // 권한 UNI_STUDENT, POST_STUDENT, PROFESSOR, OFFICE
-    private String name; // 회원의 이름
-    private String password;
-    private int cnt; // 회원 예약 가능 횟수
 
     public MemberDto(Member loginMember) {
         this.studentNo = loginMember.getStudentNo();
         this.id = loginMember.getId();
-        this.major = loginMember.getMajor();
-        this.phoneNo = loginMember.getPhoneNo();
-        this.authority = loginMember.getAuthority();
-        this.name = loginMember.getName();
-        this.password = loginMember.getPassword();
-        this.cnt = loginMember.getCnt();
     }
 
 }
--- back/reserve/src/main/java/sejong/reserve/dto/RoomDto.java
+++ back/reserve/src/main/java/sejong/reserve/dto/RoomDto.java
@@ -14,41 +14,10 @@ import javax.persistence.Column;
 public class RoomDto {
     private Long id; // 회의실 id
 
-    private String name; // 회의실 이름
 
-    private String loc; // 회의실 위치
-
-    private int cap; // 수용인원
-
-    private String info; // 회의실 상세정보
-
-    private int board; // 화이트보드 개수
-
-    private int tv; // 티비 개수
-
-    private int com; // 컴퓨터 개수
-
-    private String picture; // 사진 경로
-
-    private Boolean empty; // 회의실 예약 가능 여부
-
-    private Boolean wifi; // 와이파이 여부
-
-    private Boolean bim_projector; // 빔프로젝터 여부
 
     public RoomDto(Room roomInfo) {
         this.id = roomInfo.getId();
-        this.name = roomInfo.getName();
-        this.loc = roomInfo.getLoc();
-        this.cap = roomInfo.getCap();
-        this.info = roomInfo.getInfo();
-        this.board = roomInfo.getBoard();
-        this.tv = roomInfo.getTv();
-        this.com = roomInfo.getCom();
-        this.picture = roomInfo.getPicture();
-        this.empty = roomInfo.getEmpty();
-        this.wifi = roomInfo.getWifi();
-        this.bim_projector = roomInfo.getBim_projector();
     }
 
 }
