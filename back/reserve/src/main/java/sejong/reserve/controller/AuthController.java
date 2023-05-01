e4aaf8be78582307768edac9c616822299fcd2dc
Dingadung <1229jimin@sju.ac.kr>
2023-05-01 22:11:43 +0900

    Edit: checkLogin return 값 변경 -> login된 member의 sno

--- back/reserve/src/main/java/sejong/reserve/controller/AuthController.java
+++ back/reserve/src/main/java/sejong/reserve/controller/AuthController.java
@@ -56,14 +56,14 @@ public class AuthController {
   }
 
   @GetMapping("checkLogin")
-  public ResponseEntity<Long> checkLogin(HttpSession session) throws Exception {
+  public ResponseEntity<String> checkLogin(HttpSession session) throws Exception {
     log.info("checkLogin-test");
     Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
     log.info("member = {}", member);
     if(member == null) {
       throw new NotLoginException("로그인이 되어 있지 않은 상태 입니다!");
     } else {
-      return new ResponseEntity<>(member.getId(), HttpStatus.OK);
+      return new ResponseEntity<>(member.getStudentNo(), HttpStatus.OK);
     }
   }
 }
