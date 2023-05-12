da6488405d637cd98b6730296ac1134c9a539baa
devMingu <rooster100@naver.com>
2023-05-12 15:57:29 +0900

    Feat: 회의실 정보 연결

--- front/src/App.js
+++ front/src/App.js
@@ -59,6 +59,7 @@ function App() {
           <Route path="/AdminReservManagePage" element={<AdminReservManagePage />}></Route>
           <Route path="/AdminRoomInfoPage/:id" element={<AdminRoomInfoPage />} />
           <Route path="/AdminRoomModifyPage" element={<AdminRoomModifyPage />}></Route>
+          <Route path="/RoomInfo" element={<RoomInfo />}></Route>
         </Routes>
       </RootContainer>
     </BrowserRouter>
