658cb549cc028e29f54cc6d47ed9277b14e395c1
Dingadung <1229jimin@sju.ac.kr>
2023-05-15 23:44:32 +0900

    Edit: management 컬럼 첫번째걸로 기능하게 수정

--- back/reserve/src/main/java/sejong/reserve/repository/ManagementRepository.java
+++ back/reserve/src/main/java/sejong/reserve/repository/ManagementRepository.java
@@ -23,61 +23,61 @@ public class ManagementRepository {
     }
 
     public int getUnivCnt() {
-        return (int) em.createQuery("select m.univ_cnt from Management m").getSingleResult();
+        return (int) em.createQuery("select m.univ_cnt from Management m").getResultList().get(0);
     }
 
     public int getPostCnt() {
-        return (int) em.createQuery("select m.post_cnt from Management m").getSingleResult();
+        return (int) em.createQuery("select m.post_cnt from Management m").getResultList().get(0);
     }
 
     public int getOfficeCnt() {
-        return (int) em.createQuery("select m.office_cnt from Management m").getSingleResult();
+        return (int) em.createQuery("select m.office_cnt from Management m").getResultList().get(0);
     }
     public int getProCnt() {
-        return (int) em.createQuery("select m.pro_cnt from Management m").getSingleResult();
+        return (int) em.createQuery("select m.pro_cnt from Management m").getResultList().get(0);
     }
 
 
     public int getUnivHourGap() {
-        return (int) em.createQuery("select m.univ_hour_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.univ_hour_gap from Management m").getResultList().get(0);
     }
     public int getPostHourGap() {
-        return (int) em.createQuery("select m.post_hour_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.post_hour_gap from Management m").getResultList().get(0);
     }
 
     public int getOfficeHourGap() {
-        return (int) em.createQuery("select m.office_hour_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.office_hour_gap from Management m").getResultList().get(0);
     }
     public int getProHourGap() {
-        return (int) em.createQuery("select m.pro_hour_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.pro_hour_gap from Management m").getResultList().get(0);
     }
 
     public int getUnivDayGap() {
-        return (int) em.createQuery("select m.univ_day_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.univ_day_gap from Management m").getResultList().get(0);
     }
     public int getPostDayGap() {
-        return (int) em.createQuery("select m.post_day_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.post_day_gap from Management m").getResultList().get(0);
     }
 
     public int getOfficeDayGap() {
-        return (int) em.createQuery("select m.office_day_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.office_day_gap from Management m").getResultList().get(0);
     }
     public int getProDayGap() {
-        return (int) em.createQuery("select m.pro_day_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.pro_day_gap from Management m").getResultList().get(0);
     }
 
     public int getUnivWeekGap() {
-        return (int) em.createQuery("select m.univ_week_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.univ_week_gap from Management m").getResultList().get(0);
     }
     public int getPostWeekGap() {
-        return (int) em.createQuery("select m.post_week_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.post_week_gap from Management m").getResultList().get(0);
     }
 
     public int getOfficeWeekGap() {
-        return (int) em.createQuery("select m.office_week_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.office_week_gap from Management m").getResultList().get(0);
     }
     public int getProWeekGap() {
-        return (int) em.createQuery("select m.pro_week_gap from Management m").getSingleResult();
+        return (int) em.createQuery("select m.pro_week_gap from Management m").getResultList().get(0);
     }
 
 }
