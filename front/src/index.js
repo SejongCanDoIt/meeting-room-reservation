233b41da30919541b713816e2230d229ebc81ebb
devMingu <rooster100@naver.com>
2023-05-20 12:51:21 +0900

    Edit: 두번 랜더링되는 문제해결

--- front/src/index.js
+++ front/src/index.js
@@ -8,7 +8,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-      <App />  
-  </React.StrictMode>
+  <App />  
+  // <React.StrictMode>
+  //     <App />  
+  // </React.StrictMode>
 );
