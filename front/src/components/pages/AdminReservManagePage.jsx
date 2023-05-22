74fe43a940a77a794bd58fd2e47c127710e9208f
Bychan <harrrypoter@naver.com>
2023-05-22 19:45:58 +0900

    5/22 이병찬 작업

--- front/src/components/pages/AdminReservManagePage.jsx
+++ front/src/components/pages/AdminReservManagePage.jsx
@@ -5,123 +5,13 @@ import AdminSideBar from './AdminSideBar';
 import axios from "axios"
 
 export default function AdminReservManagePage() {
-    const reservations = [
-        {
-            id: 1,
-            department: '컴퓨터공학과',
-            studentId: '12345678',
-            name: '홍길동',
-            reservationTime: '2023년 3월 21일 화요일 10:00 ~ 13:00 836호'
-        },
-        {
-            id: 2,
-            department: '컴퓨터공학과',
-            studentId: '23456789',
-            name: '이병찬',
-            reservationTime: '2023년 3월 21일 화요일 14:00 ~ 17:00 836호'
-        },
-        {
-            id: 3,
-            department: '컴퓨터공학과',
-            studentId: '34567890',
-            name: '박지민',
-            reservationTime: '2023년 3월 22일 수요일 09:00 ~ 12:00 836호'
-        },
-        {
-            id: 4,
-            department: '컴퓨터공학과',
-            studentId: '45678901',
-            name: '이규훈',
-            reservationTime: '2023년 3월 22일 수요일 13:00 ~ 16:00 836호'
-        },
-        {
-            id: 5,
-            department: '컴퓨터공학과',
-            studentId: '56789012',
-            name: '김민구',
-            reservationTime: '2023년 3월 23일 목요일 10:00 ~ 13:00 836호'
-        },
-        {
-            id: 6,
-            department: '컴퓨터공학과',
-            studentId: '67890123',
-            name: '마리오',
-            reservationTime: '2023년 3월 23일 목요일 14:00 ~ 17:00 836호'
-        },
-        {
-            id: 7,
-            department: '컴퓨터공학과',
-            studentId: '78901234',
-            name: '루이지',
-            reservationTime: '2023년 3월 24일 금요일 09:00 ~ 12:00 836호'
-        },
-        {
-            id: 8,
-            department: '컴퓨터공학과',
-            studentId: '89012345',
-            name: '피이치',
-            reservationTime: '2023년 3월 24일 금요일 13:00 ~ 16:00 836호'
-        },
-        {
-            id: 1,
-            department: '컴퓨터공학과',
-            studentId: '12345678',
-            name: '홍길동',
-            reservationTime: '2023년 3월 21일 화요일 10:00 ~ 13:00 836호'
-        },
-        {
-            id: 2,
-            department: '컴퓨터공학과',
-            studentId: '23456789',
-            name: '이병찬',
-            reservationTime: '2023년 3월 21일 화요일 14:00 ~ 17:00 836호'
-        },
-        {
-            id: 3,
-            department: '컴퓨터공학과',
-            studentId: '34567890',
-            name: '박지민',
-            reservationTime: '2023년 3월 22일 수요일 09:00 ~ 12:00 836호'
-        },
-        {
-            id: 4,
-            department: '컴퓨터공학과',
-            studentId: '45678901',
-            name: '이규훈',
-            reservationTime: '2023년 3월 22일 수요일 13:00 ~ 16:00 836호'
-        },
-        {
-            id: 5,
-            department: '컴퓨터공학과',
-            studentId: '56789012',
-            name: '김민구',
-            reservationTime: '2023년 3월 23일 목요일 10:00 ~ 13:00 836호'
-        },
-        {
-            id: 6,
-            department: '컴퓨터공학과',
-            studentId: '67890123',
-            name: '마리오',
-            reservationTime: '2023년 3월 23일 목요일 14:00 ~ 17:00 836호'
-        },
-        {
-            id: 7,
-            department: '컴퓨터공학과',
-            studentId: '78901234',
-            name: '루이지',
-            reservationTime: '2023년 3월 24일 금요일 09:00 ~ 12:00 836호'
-        },
-        {
-            id: 8,
-            department: '컴퓨터공학과',
-            studentId: '89012345',
-            name: '피이치',
-            reservationTime: '2023년 3월 24일 금요일 13:00 ~ 16:00 836호'
+    const handleCancelReservation = async (id) => {
+        try {
+            await axios.delete(`/reserve/${id}`);
+            setReservList(reservList.filter(reservation => reservation.id !== id));
+        } catch (error) {
+            console.error(error);
         }
-    ];
-
-    const handleCancelReservation = () => {
-        // 예약 취소 기능 구현
     };
 
     const [reservList, setReservList] = useState(null);
@@ -155,19 +45,15 @@ export default function AdminReservManagePage() {
                     </Header>
                     <ReservationList>
                         <ReservationListHeader>
-                            <span>학과</span>
-                            <span>학번</span>
-                            <span>이름</span>
+                            <span>예약자 번호</span>
                             <span>예약 시간</span>
                             <span>회의실</span>
                         </ReservationListHeader>
                         {reservList.map(reservation => (
                             <ReservationRow key={reservation.id} status={reservation.status}>
-                                <span></span>
-                                <span></span>
-                                <span></span>
+                                <span>{reservation.member_id}</span>
                                 <span>{reservation.start} ~ {reservation.end}</span>
-                                <CancelReservation onClick={handleCancelReservation}>
+                                <CancelReservation onClick={() => handleCancelReservation(reservation.id)}>
                                     예약 취소
                                 </CancelReservation>
                             </ReservationRow>
