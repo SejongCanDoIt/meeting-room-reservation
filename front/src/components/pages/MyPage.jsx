df03693b4d971e96f20ad27260b1bc44b2102078
devMingu <rooster100@naver.com>
2023-05-27 19:31:50 +0900

    Edit: 서버로부터 회의실 정보 받아와서 정보 보여주기

--- front/src/components/pages/MyPage.jsx
+++ front/src/components/pages/MyPage.jsx
@@ -37,7 +37,7 @@ export default function MyPage() {
     useEffect(() => {
         axios.get('/reserve/user-list')
             .then((res) => {
-                // console.log(res.data);
+                console.log(res.data);
                 const info = makeReserveList(res.data);
 
                 // 같은 날일때는 시간이 앞선 것부터.
@@ -79,6 +79,9 @@ export default function MyPage() {
                 endMinute: endTime[1],
                 room_id: el.room_id,
                 isExpire: isExpire,
+                buildingName: el.room_building_name,
+                roomName: el.room_name,
+
             }
             infoData.push(info);
             // console.log(info);
@@ -95,7 +98,7 @@ export default function MyPage() {
             const date = parseInt(el.date);
             // console.log(moment(new Date(year, month, date)).format("YYYY-MM-DD"), moment(new Date()).format("YYYY-MM-DD"));
             if (moment(new Date(year, month, date)).format("YYYY-MM-DD") === moment(new Date()).format("YYYY-MM-DD") && !el.isExpire) {
-                message = `${el.room_id}호 ${el.startHour}:${el.startMinute} ~ ${el.endHour}:${el.endMinute}분에 오늘 예약이 있어요`;
+                message = `${el.buildingName} ${el.roomName}호 ${el.startHour}시${el.startMinute}분 ~ ${el.endHour}시${el.endMinute}분 오늘 예약이 있어요`;
             }
         }
         return message;
@@ -105,7 +108,7 @@ export default function MyPage() {
         let message = "최근 예약내역이 없습니다."
         for (let el of reserveList) {
             if (el.isExpire) {
-                return `${el.room_id}호 ${el.year}년 ${el.month}월 ${el.date}일 ${el.day}요일 ${el.startHour}:${el.startMinute} ~ ${el.endHour}:${el.endMinute}`;
+                return `${el.buildingName} ${el.roomName}호 ${el.year}년 ${el.month}월 ${el.date}일 ${el.day}요일 ${el.startHour}시${el.startMinute}분 ~ ${el.endHour}시${el.endMinute}분`;
             }
         }
         return message;
